fa5764a6e998cc04735c962067609aefead1ea06
howeltri <tristan.howell@gmail.com>
2020-05-08 19:45:22 -0400

    Stupid comment basics, really starting from scratch again for my thousandth time learning to code

--- day1.py
+++ day1.py
@@ -1,5 +1,15 @@
 print("giving it a shot with pycharm")
 pythonVar = "how is it?"
 print(pythonVar + " not too shabby!")
+"Multiline comment - they always throw me off in pythonMultiline comment - " \
+"they always throw me off in pythonMultiline comment - they always throw me " \
+"off in pythonMultiline comment - they always throw me off in pythonMultiline" \
+" comment - they always throw me off in pythonMultiline comment - they always" \
+" throw me off in pythonMultiline comment - they always throw me off in python" \
+"Multiline comment - they always throw me off in pythonMultiline comment - t" \
+"hey always throw me off in pythonMultiline comment - they always throw me " \
+"off in python"
+
+print("still good")
 
 
