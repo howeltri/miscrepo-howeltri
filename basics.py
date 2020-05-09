e77a59e2bc8c44949f286640aa92b5057b8d8989
howeltri <tristan.howell@gmail.com>
2020-05-09 12:00:28 -0400

    Lots of new notes in basics.py

--- basics.py
+++ basics.py
@@ -18,4 +18,32 @@
 # range(1, 10, 2) odds 1 to 10
 # range(7, 0, -1) decrement
 # break out of loops with: break
-
+# list = [] ~ len(list) - OR ~ list(range(1,4))
+# to decrement in lists its just list[-1] etc
+# Remember the in method
+#------------List Basics-----------------------
+# Items in lists are mutable
+# .append() - one item at the end to append (if you passed a list it would be a nested value
+# .insert(<insertionPosition>, <val>) -
+# .extend() - used for multiple items or to add another list
+# .clear() - removes everything
+# .pop(<index> or nothing) - removes last item by default or item at index specified
+# .remove(<value>) - removes first occurrence of that value, throws error if not found
+# .index(<value>) - returns index of the passed value
+# .count(<value>) - nmumber of times value appears in list
+# .reverse()
+# .sort() - ascending order in place
+# .join() - use as "<value>".join(<list>) to join together seperated by passed value
+# Slices!
+# list[start:end:step] - : need to be included or else it'll just return val at that index
+# ex: list[1:] - index 1 to end OR list[-3:] returns last three values
+# can make a copy in dif mem space with list2 = list[:]
+# ex: list[2:4] or list[:3]
+# finally count by 2 across a range ~ list[:10:2]
+# reverse a list ~ list_r = list[::-1]
+# can swap in lists by doing names[0], names[1] = names[1], names[0]
+# --------------List Comprehension-----------------------
+# new_list_from_old = [num * 2 for num in numbers]
+# [<your_operation> for <item_to_operate_on> in <list>]
+# You can use conditional logic in there too!
+# list2 = [num for num in numbers if num is True]
